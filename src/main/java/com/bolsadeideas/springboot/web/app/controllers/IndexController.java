7091bb7cc22ed17d0203a3f8302e7754bca08c54
Yasín Muñoz <yasincna@hotmail.com>
2021-01-03 01:22:17 +0100

    Portfolio añadido

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,8 +11,9 @@ public class IndexController {
 	//Get: consultar información en el servidor
 	
 	@GetMapping({"/index", "/", "/home"}) //http://localhost:8080/index
-	public String index() {
+	public String index(Model model) {
 		
+		model.addAttribute("titulo", "Come Mierdas");
 		return "index"; //carga la vista index
 		
 	}
